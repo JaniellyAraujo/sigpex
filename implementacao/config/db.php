37915668c4dc6161afc5404a9ebe1a9b3929248d
JaniellyAraujo <janielly300@gmail.com>
2018-12-10 12:47:36 -0200

    BD conf

--- implementacao/config/db.php
+++ implementacao/config/db.php
@@ -1,12 +1,10 @@
-<?php
+﻿<?php
 
 //CONEXÃO COM O BANCO DE DADOS
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=cleianec_sigpex',
-    'username' => 'root', //cleianec
-    'password' => '', //ido3FEu561
-    'charset' => 'utf8',
+    'username' => 'root',     'password' => '',     'charset' => 'utf8',
         // Schema cache options (for production environment)
         //'enableSchemaCache' => true,
         //'schemaCacheDuration' => 60,
